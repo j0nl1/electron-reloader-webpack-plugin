fc425d324f3d059c81635992fd81f471a673f69d
j0nl1 <jonlitech@gmail.com>
2020-04-30 17:04:13 +0200

    Added protection types for arguments

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,30 @@ import { spawn, ChildProcess } from "child_process";
 import { Compiler } from "webpack";
 
 export = class ElectronReloader {
-  command: string;
-  args: string[];
+  command?: string;
+  args?: string[];
   activeProcess?: ChildProcess;
   constructor(command: string, args: string[]) {
-    this.command = command;
-    this.args = args;
+    this.assignParameters(command, args);
   }
   apply(compiler: Compiler): void {
     compiler.hooks.afterCompile.tap("electronReloader", () => {
       if (this.activeProcess) this.activeProcess.kill();
-      this.activeProcess = spawn(this.command, this.args, {
+      this.activeProcess = spawn(this.command as string, this.args as string[], {
         shell: true,
         env: process.env,
         stdio: "inherit",
       });
     });
+  }
+  assignParameters(command: string, args: string[]) {
+    if (typeof command !== 'string') {
+      throw new Error('Your command have to be a string');
+    } 
+    if (!Array.isArray(args)) {
+      throw new Error('Your args have to be contain in array');
+    }
+    this.command = command;
+    this.args = args;
   }
 };
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,44 @@
 import ElectronReloader from "../src/index";
+import child_process, { ChildProcess } from 'child_process';
+import { Compiler } from "webpack";
+
+const spawnOptions = { shell: true, env: process.env, stdio: "inherit" }
+
+const compilerMock = { hooks: { afterCompile: {} } } as Compiler
+
+compilerMock.hooks.afterCompile.tap = (name: any, fn: Function) => { fn() };
 
 describe("Electron Reloader", () => {
+    let spySpawn: unknown;
+    beforeEach(() => {
+      spySpawn = jest.spyOn(child_process, 'spawn').mockReturnValue({kill: () => true} as ChildProcess);
+    })
   it("should be a instance of a function", () => {
     expect(ElectronReloader).toBeInstanceOf(Function);
+  });
+  it("should throw an error if command isn't a string", () => {
+      const instance = () => new ElectronReloader(null as any, []);
+      expect(instance).toThrowError('Your command have to be a string');
+  });
+  it("should throw an error if args isn't an array", () => {
+      const instance = () => new ElectronReloader('npm', 'TEST' as any);
+      expect(instance).toThrowError('Your args have to be contain in array');
+  });
+  it("should call spawn method with the arguments provided", () => {
+      const instance = new ElectronReloader('npm', ['run', 'electron:start']);
+      instance.apply(compilerMock)
+      expect(spySpawn).toHaveBeenCalledWith('npm', ['run', 'electron:start'], spawnOptions);
+  });
+  it("should assign spawn process to activeProcess property", () => {
+      const instance = new ElectronReloader('npm', ['run', 'electron:start']);
+      instance.apply(compilerMock)
+      expect(instance.activeProcess).toHaveProperty('kill');
+  });
+  it("should kill activeProcess and create a new spawn process", () => {
+      const instance = new ElectronReloader('npm', ['run', 'electron:start']);
+      instance.apply(compilerMock)
+      const spyActiveProcess = jest.spyOn(instance.activeProcess as ChildProcess, 'kill');
+      instance.apply(compilerMock)
+      expect(spyActiveProcess).toHaveBeenCalled();
   });
 });
